c0e22878d9a205f5e8e2cfe5a3bde7443a2cc6ac
Jehan Zeb <101641007+JehanZeb832@users.noreply.github.com>
2025-11-07 00:53:11 +0500

    Add files via upload

---
+++ app/ajax/user_data.php
@@ -0,0 +1,83 @@
+<?php
+require_once '../init.php';
+
+## Read value
+$draw = $_POST['draw'];
+$row = $_POST['start'];
+$rowperpage = $_POST['length']; // Rows display per page
+$columnIndex = $_POST['order'][0]['column']; // Column index
+$columnName = $_POST['columns'][$columnIndex]['data']; // Column name
+$columnSortOrder = $_POST['order'][0]['dir']; // asc or desc
+$searchValue = $_POST['search']['value']; // Search value
+
+$searchArray = array();
+
+## Search 
+$searchQuery = " ";
+if($searchValue != ''){
+   $searchQuery = " AND (id LIKE :id or 
+        username LIKE :username OR 
+        user_role LIKE :user_role ) ";
+   $searchArray = array( 
+        'id'=>"%$searchValue%", 
+        'username'=>"%$searchValue%",
+        'user_role'=>"%$searchValue%"
+   );
+}
+
+## Total number of records without filtering
+$stmt = $pdo->prepare("SELECT COUNT(*) AS allcount FROM user ");
+$stmt->execute();
+$records = $stmt->fetch();
+$totalRecords = $records['allcount'];
+
+## Total number of records with filtering
+$stmt = $pdo->prepare("SELECT COUNT(*) AS allcount FROM user WHERE 1 ".$searchQuery);
+$stmt->execute($searchArray);
+$records = $stmt->fetch();
+$totalRecordwithFilter = $records['allcount'];
+
+## Fetch records
+$stmt = $pdo->prepare("SELECT * FROM user WHERE 1 ".$searchQuery." ORDER BY ".$columnName." ".$columnSortOrder." LIMIT :limit,:offset");
+
+// Bind values
+foreach($searchArray as $key=>$search){
+   $stmt->bindValue(':'.$key, $search,PDO::PARAM_STR);
+}
+
+$stmt->bindValue(':limit', (int)$row, PDO::PARAM_INT);
+$stmt->bindValue(':offset', (int)$rowperpage, PDO::PARAM_INT);
+$stmt->execute();
+$empRecords = $stmt->fetchAll();
+
+$data = array();
+
+foreach($empRecords as $row){
+   $data[] = array(
+      "id"=>$row['id'],
+      "username"=>$row['username'],
+      "user_role"=>$row['user_role'],
+      "action"=>'
+          <div class="btn-group">
+          <button type="button" class="btn btn-primary btn-sm rounded-0 dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+            Action
+          </button>
+          <div class="dropdown-menu dropdown-menu-right">
+            <a href="index.php?page=edit_user&&edit_id='.$row['id'].'" class="dropdown-item" type="button">edit user</a>
+            <a href="index.php?page=userRole&&edit_id='.$row['id'].'" class="dropdown-item" type="button">assign role</a>
+            <a href="" class="dropdown-item" id="user_delete_btn" data-id="'.$row['id'].'" type="button">delete user</a>
+          </div>
+        </div>
+      ',
+   );
+}
+
+## Response
+$response = array(
+   "draw" => intval($draw),
+   "iTotalRecords" => $totalRecords,
+   "iTotalDisplayRecords" => $totalRecordwithFilter,
+   "aaData" => $data
+);
+
+echo json_encode($response);
---
+++ app/config/config.php
@@ -0,0 +1,21 @@
+<?php
+// setting time zone
+date_default_timezone_set("Asia/Karachi");
+
+// define the site root
+define('SITE_ROOT', 'http://localhost/WarehouseManagementSystem/');
+
+
+
+// Database Information
+// Database Hostname
+define('DATABASE_HOST','localhost');
+// Database Username
+define('DATABASE_USER','root');
+// Database Name
+define('DATABASE_NAME', 'warehousedb');
+// Database DB_PASS
+define('DATABASE_PASS','');
+
+
+ ?>
